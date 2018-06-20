81bf6084e6ef7c1a9c2d99bb0fd7f0f836066ab7
Shehbab Kakkar <shehbab1@gmail.com>
2018-06-20 21:53:56 +0530

    RepoTech Updates

---
+++ date-time.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.6
+from datetime import datetime
+now = datetime.now()
+d=now.date()
+print(now)
+print(now.date())
+print(now.time())
+print(now.second)
+print(now.hour)
+print(d.day)
+print(d.month)
+print(d.year)
+print(d.weekday())
