e66ea1e0b5b90b9c2546fb25049cc0c8da3a7136
Shehbab Kakkar <shehbab1@gmail.com>
2018-07-01 11:05:56 +0530

    update

---
+++ Classes/self.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.6
+class Company:
+      def employee(self):
+           pass
+     
+company01 = Company()
+company01.employee()  #Actually mentioned below
+
+#Class.function(object)
+'''
+Class = Company
+function = employee
+object = company
+
+Company.employee(company01)
+'''
