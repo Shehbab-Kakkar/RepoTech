6f24e2a3953b43795419b926c1c8c234a806958d
Shehbab Kakkar <shehbab1@gmail.com>
2018-07-01 19:05:37 +0530

    update

--- Classes/staticmethod.py
+++ Classes/staticmethod.py
@@ -4,9 +4,17 @@ class Employee:
            self.name = "Ben"
            print("Name = ",self.name)
       
-      def welcomeMessage(self):
+      @staticmethod  #declarator
+      def welcomeMessage():
            print("Welcome to our organization!")
 employee = Employee()
 employee.employeeDetails()   # object.Method
 Employee.employeeDetails(employee)   # class.Method(object)
+employee.welcomeMessage()
 
+
+#declarator called statemethod
+#declarator are function which task another function and extends 
+#there functionality
+'''staticmethod  ignore the binding of object'''
+#statemethod ignore self parameter 
